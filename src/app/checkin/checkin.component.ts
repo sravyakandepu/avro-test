fc41bb8302f6f38e2dcffd10097574d735f8717d
sravyakandepu <sravya.sweet123@gmail.com>
2019-01-21 16:24:24 -0600

    git commit -n "changes to add the view all checks"

--- src/app/checkin/checkin.component.ts
+++ src/app/checkin/checkin.component.ts
@@ -9,8 +9,11 @@ import {CheckinService} from './checkin.service';
 export class CheckinComponent implements OnInit {
     orderItems: [] = [];
     private itemsList: [] = [];
+    private tempItemsList: [] = [];
+    private checkWiseItemList: [] = [];
     private displayItemTable: boolean;
     private displayOrderItemTable: boolean;
+    private closedCheck: any;
 
     constructor(private checkinService: CheckinService) {
     }
@@ -18,7 +21,7 @@ export class CheckinComponent implements OnInit {
     tableData: [] = [];
     private checkObjct: { tableId: string };
     tableId: any;
-    checksByTableId: any;
+    checksByTableId: any = [];
     alreadyCalledForCheck = [];
 
     @ViewChildren('checkboxes') checkboxes: QueryList<ElementRef>;
@@ -28,8 +31,12 @@ export class CheckinComponent implements OnInit {
     }
 
     getTables(): void {
+        const self = this;
         this.checkinService.getTables().subscribe(tables => {
             this.tableData = tables;
+            this.checkinService.getItems().subscribe(itemResponse => {
+                self.tempItemsList = itemResponse;
+            });
         });
     }
 
@@ -44,25 +51,66 @@ export class CheckinComponent implements OnInit {
         }
     }
 
-    getCheckByTableId(table) {
+    getCheckByTableId(table, isClickOnView) {
         this.displayItemTable = this.displayOrderItemTable = false;
         let checkIndex: number;
-        this.checksByTableId = undefined;
+        this.checksByTableId = [];
+        this.orderItems = [];
+        this.closedCheck = undefined;
         this.checkinService.getCheckByTableId().subscribe(checks => {
             this.tableId = table;
             for (checkIndex = 0; checkIndex < checks.length; checkIndex++) {
+                const checkWiseItem: object = {
+                    checkId: checks[checkIndex].id,
+                    itemList: this.tempItemsList.concat()
+                };
+                let isCheckPresent = false;
+                for (let index = 0; index < this.checkWiseItemList.length; index++) {
+                    // @ts-ignore
+                    if (this.checkWiseItemList[index].checkId === checks[checkIndex].id) {
+                        isCheckPresent = true;
+                        break;
+                    }
+                }
+                if (!isCheckPresent) {
+                    // @ts-ignore
+                    this.checkWiseItemList.push(checkWiseItem);
+                }
+
                 if (checks[checkIndex].tableId !== table.id) {
+                    checks.splice(checkIndex, 1);
+                    checkIndex--;
+                } else if (isClickOnView && checks[checkIndex].closed) {
                     checks.splice(checkIndex, 1);
                     checkIndex--;
                 }
             }
             this.checksByTableId = checks;
+            if (this.orderItems.length) {
+                // @ts-ignore
+                for (let orderItemIndex = 0; orderItemIndex < this.orderItems.length; orderItemIndex++) {
+                    // @ts-ignore
+                    this.tempItemsList.map((item) => {
+                        // @ts-ignore
+                        if (item.id === this.orderItems[orderItemIndex].itemId) {
+                            // @ts-ignore
+                            this.orderItems[orderItemIndex].name = item.name;
+                        }
+                    });
+                }
+            }
         });
     }
 
     closeCheckById(checkId) {
+        this.displayOrderItemTable = false;
         this.checkinService.closeCheck(checkId).subscribe(closeCheckResponse => {
-            this.checksByTableId = [closeCheckResponse];
+            for (let index = 0; index < this.checksByTableId.length; index++) {
+                if (this.checksByTableId[index].id === closeCheckResponse.id) {
+                    this.checksByTableId[index] = closeCheckResponse;
+                    this.closedCheck = closeCheckResponse;
+                }
+            }
         });
     }
 
@@ -72,27 +120,54 @@ export class CheckinComponent implements OnInit {
         this.alreadyCalledForCheck = [];
         this.displayOrderItemTable = false;
         this.itemsList = [];
+        this.checkWiseItemList = [];
         this.uncheckAll();
         this.checkinService.deleteAllChecks().subscribe(deleteChecks => {
-            console.log('delete checks', deleteChecks);
         });
     }
 
     getCheckById(checkId) {
         this.displayOrderItemTable = true;
         this.displayItemTable = false;
+        this.closedCheck = undefined;
         this.checkinService.getCheck(checkId).subscribe(getCheckResponse => {
-            console.log('here dat', getCheckResponse);
-            this.checksByTableId = [getCheckResponse];
-            this.orderItems = this.checksByTableId[0].orderedItems;
+            let tempResponse = [getCheckResponse];
+            for (let index = 0; index < this.checksByTableId.length; index++) {
+                if (this.checksByTableId[index].id === getCheckResponse.id) {
+                    this.checksByTableId[index] = getCheckResponse;
+                }
+            }
+            for (let orderItemIndex = 0; orderItemIndex < tempResponse[0].orderedItems.length; orderItemIndex++) {
+                this.tempItemsList.map((item) => {
+                    // @ts-ignore
+                    if (item.id === tempResponse[0].orderedItems[orderItemIndex].itemId) {
+                        // @ts-ignore
+                        tempResponse[0].orderedItems[orderItemIndex].name = item.name;
+                    }
+                });
+            }
+            for (let index = 0; index < this.checksByTableId.length; index++) {
+                // @ts-ignore
+                if (this.checksByTableId[index].id === tempResponse.id) {
+                    this.checksByTableId[index] = tempResponse;
+                }
+            }
+            this.closedCheck = tempResponse[0];
+            console.log('checksByTableId.length', this.closedCheck)
+            this.orderItems = tempResponse[0].orderedItems.reverse();
         });
     }
 
     getItems() {
         this.displayItemTable = true;
         this.displayOrderItemTable = false;
-        this.checkinService.getItems().subscribe(itemResponse => {
-            this.itemsList = itemResponse;
+        this.itemsList = [];
+        this.checkWiseItemList.map((itemObj) => {
+            // @ts-ignore
+            if (itemObj.checkId === this.checksByTableId[0].id) {
+                // @ts-ignore
+                this.itemsList.push.apply(this.itemsList, itemObj.itemList);
+            }
         });
     }
 
@@ -102,10 +177,6 @@ export class CheckinComponent implements OnInit {
             itemId: itemId
         };
         this.checkinService.addItem(itemObj, this.checksByTableId[0].id).subscribe(itemResponse => {
-            self.itemsList.splice(self.itemsList.findIndex(item => {
-                // @ts-ignore
-                return item.id === itemId;
-            }), 1);
         });
     }
 
@@ -114,7 +185,6 @@ export class CheckinComponent implements OnInit {
         const itemObj = {
             orderedItemId: itemId
         };
-        console.log('her', itemId, this.checksByTableId[0].id);
         this.checkinService.voidItem(itemObj, this.checksByTableId[0].id).subscribe(voidItemResponse => {
             self.getCheckById(self.checksByTableId[0].id);
         });
